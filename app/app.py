e283ad5e1e10c93635aeb25dbf166a48be371300
Пресиан Андрейчев <138018616+PresAndreichev@users.noreply.github.com>
2025-01-09 17:04:53 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, render_template
 from sudopy import Sudoku, Generate
+import ctypes
 
 app = Flask(__name__)
 
-# The 2D list of the starting numbers in the Sudoku
+# the 2D list of the starting numbers in the sudoku
 play_list = []
 
-# Boolean that is True if a Sudoku has been generated using
-# the Generate object, False otherwise
+# boolean that is True if a Sudoku has been generated using
+#   the Generate object, False otherwise
 has_gen = False
-
 
 @app.route('/')
 def menu():
@@ -33,86 +33,88 @@ def play():
     global play_list
     global has_gen
     sudoku_grid_list = []
-
     if request.method == 'POST':
-        # If the POST request is from '/input_play' or '/play'
+
+        # if the POST request is from '/input_play' or '/play'
         if 'play' in request.referrer:
-            # Build 2D list to initialize Sudoku object
+
+            # build 2D list in order to initialize Sudoku object
             for i in range(9):
                 row_list = []
                 for j in range(9):
-                    cell_id = f"{i * 9 + j}"
-                    content = request.form[cell_id]
-                    row_list.append(0 if content == "" else int(content))
+                    id = "" + str(i*9 + j)
+                    content = request.form[id]
+                    if content == "":
+                        row_list.append(0)
+                    else:
+                        row_list.append(int(content))
                 sudoku_grid_list.append(row_list)
             s = Sudoku(sudoku_grid_list)
 
-            # If the POST request is from '/input_play'
+            # if the POST request is from '/input_play"
             if '/input_play' in request.referrer:
                 play_list = s.return_array()
-                return render_template(
-                    'play.html', 
-                    is_solved=None, 
-                    input_array=play_list, 
-                    play_array=None
-                )
 
-            # If the POST request is from '/play'
+                # renders the play.html file
+                # is_solved will be "None" since the sudoku is not completed
+                # input_array is initialized to play_list, which contains the starting numbers of a sudoku
+                # play_array refers to the compiled 2D list after a user has finished playing a sudoku; in this case, play_array will be None
+                return render_template('play.html', is_solved = "None", input_array = play_list, play_array = None)
+
+            # if the POST request is from '/play" (when the user checks their sudoku)
             elif '/play' in request.referrer:
-                return render_template(
-                    'play.html', 
-                    is_solved=s.check_grid(), 
-                    check_array=s.check_grid_items(), 
-                    input_array=play_list, 
-                    play_array=s.return_array()
-                )
+                # renders the play.html file
+                # is_solved is initialized to a boolean of whether the completed sudoku is valid
+                # check_array is initialized to a 2D list of booleans that signify if each cell is valid
+                # input_array is initialized to play_list, which contains the starting numbers of a sudoku
+                # play_array refers to the compiled 2D list after a user has finished playing the sudoku
+                return render_template('play.html', is_solved = s.check_grid(), check_array = s.check_grid_items(), input_array = play_list, play_array = s.return_array())
 
-        # If the POST request is from the menu page
+        # if the POST request is from the menu page
         elif '/' in request.referrer:
-            difficulty = (
-                request.form.get('easy') or
-                request.form.get('medium') or
-                request.form.get('hard') or
-                request.form.get('expert')
-            )
 
-            # Generate Sudoku of the specified difficulty
+            # obtain the user specified difficulty
+            if request.form.get('easy') != None:
+                content = request.form.get('easy')
+            elif request.form.get('medium') != None:
+                content = request.form.get('medium')
+            elif request.form.get('hard') != None:
+                content = request.form.get('hard')
+            else:
+                content = request.form.get('expert')
+
+            # if a Sudoku hasn't already been generated, create one of the specified difficulty
             if not has_gen:
-                g = Generate(difficulty)
+                g = Generate(content)
                 play_list = g.generate_sudoku()
                 has_gen = True
-            return render_template(
-                'play.html', 
-                is_solved=None, 
-                input_array=play_list, 
-                play_array=None
-            )
+            return render_template('play.html', is_solved = "None", input_array = play_list, play_array=None)
     else:
-        return render_template(
-            'play.html', 
-            is_solved=None, 
-            input_array=play_list, 
-            play_array=None
-        )
+        return render_template('play.html', is_solved="None", input_array = play_list, play_array = None)
 
 
 @app.route('/solution', methods=['POST', 'GET'])
 def solution():
     sudoku_grid_list = []
     if request.method == 'POST':
-        # Build 2D list to initialize Sudoku object
+
+        # build 2D list from inputted numbers in order to initialize Sudoku object
         for i in range(9):
             row_list = []
             for j in range(9):
-                cell_id = f"{i * 9 + j}"
-                content = request.form[cell_id]
-                row_list.append(0 if content == "" else int(content))
+                id = "" + str(i*9 + j)
+                content = request.form[id]
+                if content == "":
+                    row_list.append(0)
+                else:
+                    row_list.append(int(content))
             sudoku_grid_list.append(row_list)
 
-        # Create the solved Sudoku
+        # Create the solved Sudoku based off of the numbers inputted by the user in '/input_solve'
         s = Sudoku(sudoku_grid_list)
         s.solve()
-        return render_template('solution.html', solved_array=s.return_array())
+
+        return render_template('solution.html', solved_array = s.return_array())
     else:
         return render_template('menu.html')
 
